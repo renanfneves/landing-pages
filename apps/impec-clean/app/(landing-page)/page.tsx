148e6358474c28635d6e2c4ef6558456cda9471b
Renan Floriano Neves <renanfneves@gmail.com>
2023-09-05 17:12:16 +0100

    feat: update layout global

--- apps/impec-clean/app/(landing-page)/page.tsx
+++ apps/impec-clean/app/(landing-page)/page.tsx
@@ -1,14 +1,10 @@
-import { Button } from "@landing-pages/ui-library"
 import { HomeHero } from "../../components/HomeHero";
 
 export default async function LandingPage() {
   return (
     <main>
       <HomeHero />
-      <div className="max-width">
-        <Button size="lg">
-          hello world
-        </Button>
+      <div className="padding-x padding-y max-width">
       </div>
     </main>
   );
