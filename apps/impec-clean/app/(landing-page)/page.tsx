969110cd9649e5cc9f42a162c034ce44cb0575eb
Renan Floriano Neves <renanfneves@gmail.com>
2023-09-05 17:13:05 +0100

    feat: add keen-slider plugin

--- apps/impec-clean/app/(landing-page)/page.tsx
+++ apps/impec-clean/app/(landing-page)/page.tsx
@@ -1,10 +1,12 @@
 import { HomeHero } from "../../components/HomeHero";
+import { Testimonials } from "../../components/Testimonials";
 
 export default async function LandingPage() {
   return (
     <main>
       <HomeHero />
       <div className="padding-x padding-y max-width">
+        <Testimonials />
       </div>
     </main>
   );
