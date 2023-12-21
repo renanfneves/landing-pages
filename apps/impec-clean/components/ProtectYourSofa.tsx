763edc4c0473b8737cbd965bd28259d85aac594d
Renan Floriano Neves <renanfneves@gmail.com>
2023-12-21 09:02:59 -0300

    feat: set max layout

--- apps/impec-clean/components/ProtectYourSofa.tsx
+++ apps/impec-clean/components/ProtectYourSofa.tsx
@@ -22,8 +22,15 @@ export function ProtectYourSofa() {
           e sofisticação, mesmo nos momentos mais imprevisíveis.
         </p>
         <section className="w-full flex justify-center">
-          <Button variant="secondary" size="lg">
-            QUERO PROTEGER O MEU SOFÁ!
+          <Button variant="secondary" size="lg" asChild>
+            <a
+              className="flex-center flex-col gap-4 font-bold text-white"
+              href="http://wa.me/+351932270602?text=Olá%20equipa%20Impec%20Clean,%20vim%20através%20do%20site.%0A%0AGostaria%20de%20agendar%20uma%20higienização%20para%20o%20meu%20sofá"
+              target="_blank"
+              rel="noreferrer"
+            >
+              QUERO PROTEGER O MEU SOFÁ!
+            </a>
           </Button>
         </section>
       </GraphicArticle.ContentRoot>
