189756b2b4f79ebc770ecedc5383ed72cc712423
Renan Floriano Neves <renanfneves@gmail.com>
2024-01-17 19:36:42 +0000

    feat: impec team photo

--- apps/impec-clean/components/ImpecTeam.tsx
+++ apps/impec-clean/components/ImpecTeam.tsx
@@ -1,13 +1,21 @@
 import { GraphicArticle } from '@landing-pages/ui-library'
-import BackgroundImage from '../assets/team-image.svg'
+import BackgroundImage from '../assets/team-image.webp'
+import Image from 'next/image'
 
 export function ImpecTeam() {
   return (
-    <GraphicArticle.Root className="flex-col-reverse">
-      <GraphicArticle.Image
-        src={BackgroundImage.src}
-        description="Serviços oferecidos pela Impec Clean"
-      />
+    <GraphicArticle.Root className="flex-col-reverse justify-center">
+      <figure className="relative w-[18rem] h-[18rem] md:w-[20rem] md:h-[20rem] rounded-full overflow-hidden">
+        <Image
+          fill
+          src={BackgroundImage.src}
+          alt="Serviços oferecidos pela Impec Clean"
+          priority={true}
+          sizes="100vw"
+          className="object-cover rounded-md aspect-video"
+        />
+      </figure>
+
       <GraphicArticle.ContentRoot>
         <h3 className="text-sm font-bold">Profissionalismo</h3>
         <h2>A Força por Trás da Impec Clean</h2>
--- libs/ui-library/src/components/ui/GraphicArticle/GraphicArticleImage.tsx
+++ libs/ui-library/src/components/ui/GraphicArticle/GraphicArticleImage.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import { Suspense } from 'react'
 
 interface GraphicArticleImageProps {
   src: string
@@ -12,16 +11,14 @@ export function GraphicArticleImage({
 }: GraphicArticleImageProps) {
   return (
     <figure className="relative w-full h-80 md:w-1/2 md:h-[500px]">
-      <Suspense>
-        <Image
-          fill
-          src={src}
-          alt={description}
-          priority={true}
-          sizes="100vw 50vw"
-          className="object-cover rounded-md aspect-video"
-        />
-      </Suspense>
+      <Image
+        fill
+        src={src}
+        alt={description}
+        priority={true}
+        sizes="100vw 50vw"
+        className="object-cover rounded-md aspect-video"
+      />
     </figure>
   )
 }
