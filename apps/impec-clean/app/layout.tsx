54cad396830ebe07ddfd460b1ce197e7b8ad763f
Renan Floriano Neves <renanfneves@gmail.com>
2024-01-23 12:12:02 +0000

    feat: add pixels

--- apps/impec-clean/app/layout.tsx
+++ apps/impec-clean/app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { Roboto_Flex as Roboto } from 'next/font/google'
 import { ImpecHeader } from '../components/ImpecHeader'
 import { Footer } from '../components/Footer'
@@ -18,6 +19,42 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-314025232');
+              `,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '317290567186879');
+              fbq('track', 'PageView');
+              `,
+          }}
+        />
+        <img
+          height="1"
+          width="1"
+          alt=""
+          style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=317290567186879&amp;ev=PageView&amp;noscript=1"
+        />
+      </head>
+
       <body
         className={`relative ${roboto.className} overflow-x-hidden`}
         suppressHydrationWarning={true}
